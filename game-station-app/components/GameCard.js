3a6e9257cc7a1b00b24b61ad25b9489a64307281
lucaspanwang <wang.pan1@northeastern.edu>
2022-06-18 16:05:04 -0700

    Finished the static contests of a game card

--- game-station-app/components/GameCard.js
+++ game-station-app/components/GameCard.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function GameCard({ infor, navigation }) {
   return (
     <View style={styles.cardContainer}>
       <Image source={{ uri: infor.picture_urls[0] }} style={styles.image} />
-      <Text style={styles.title}>{infor.title}</Text>
-      <Text>haha</Text>
-      <Text>haha</Text>
+      <View style={styles.inforContainer}>
+        <Text style={styles.title}>{infor.title}</Text>
+        <Text>{infor.post_date.slice(0, 10)}</Text>
+        <Text>Postal Code: {infor.postal_code}</Text>
+        <Text>Price: C${infor.price}</Text>
+        <TouchableOpacity style={styles.bubble}>
+          <Text style={styles.buttonText}>DETAILS</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -35,12 +41,32 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 8,
     width: "100%",
     height: 250,
-    marginBottom: 8,
+  },
+
+  inforContainer: {
+    width: "90%",
+    marginLeft: "5%",
+    marginTop: 10,
+    marginBottom: 12,
   },
 
   title: {
-    lineHeight: 30,
+    lineHeight: 24,
     fontWeight: "bold",
     fontSize: 16,
+  },
+
+  bubble: {
+    backgroundColor: "rgba(30,30,240,0.7)",
+    paddingTop: 13,
+    paddingBottom: 13,
+    marginTop: 10,
+    borderRadius: 6,
+  },
+
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
   },
 });
