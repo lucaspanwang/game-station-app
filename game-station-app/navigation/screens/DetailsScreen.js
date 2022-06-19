de19d148ed7168098a4499e9ca2ffebb5a07504f
yugao <gao.yu1@northeastern.edu>
2022-06-19 16:10:10 -0700

    detail img css changed

--- game-station-app/navigation/screens/DetailsScreen.js
+++ game-station-app/navigation/screens/DetailsScreen.js
@@ -22,7 +22,33 @@ export default function DetailsScreen(props) {
           width: '100%',
           height: 400,
         }}
+
+        blurRadius={10}
       />
+      <View
+        style={{
+          width: '80%',
+          marginLeft: '10%',
+          marginTop: '10%',
+          height: 300,
+          position: 'absolute',
+          shadowColor: "black",
+          shadowOffset: { height: 2 },
+          shadowOpacity: 0.3,
+        }}
+      >
+        <Image
+          source={{
+            uri: infor.picture_urls[0],
+          }}
+          style={{
+            width: '100%',
+            height: '100%',
+            borderRadius: 10,
+          }}
+        />
+      </View>
+
       <Text>{infor.title}</Text>
       <Button
         title="EDIT"
